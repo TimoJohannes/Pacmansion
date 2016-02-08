c0753b379431f697f56eb43ad3b0b86d9ba754b5
nikolasdas <nikolas.juergensen@web.de>
2016-02-08 23:09:02 +0000

    noch mehr visual-krams

--- Unreal-Project/Source/Pacmansion/SoundController.cpp
+++ Unreal-Project/Source/Pacmansion/SoundController.cpp
@@ -1,18 +1,25 @@
 #include "Pacmansion.h"
 #include "SoundController.h"
 
+float sfxVolume = 0.5;
+float musicVolume = 0.5;
+
 float USoundController::GetMusicVolume()
 {
-	return 1;
+	return musicVolume;
 }
 
 float USoundController::GetSFXVolume()
 {
-	return 1;
+	return sfxVolume;
 }
 
 void USoundController::SetMusicVolume(float Volume)
-{}
+{
+	musicVolume = Volume;
+}
 
 void USoundController::SetSFXVolume(float Volume)
-{}
+{
+	sfxVolume = Volume;
+}
